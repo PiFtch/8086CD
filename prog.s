4b1df5f9ea51c58aea9d5c193c3277d28dc25c83
piftch <piftch@126.com>
2019-01-04 14:24:38 +0800

    init

--- prog.s
+++ prog.s
@@ -11,7 +11,7 @@ STATE_4     EQU 4
 STATE_4_LIGHT   EQU 010100B
 
 .DATA
-MESG        DB  'TRAFFIC LIGHTS CONTROL'
+MESG        DB  'TRAFFIC LIGHTS CONTROL$'
 I8254DD0    DW  280H
 I8254DD1    DW  281H
 I8254TYPE   DW  283H                                                             
@@ -26,7 +26,7 @@ TAB         DB  3FH, 06H, 5BH, 4FH, 66H, 6DH, 7DH, 07H, 7FH, 6FH    ; 数码管�
 BZ          DB  0F1H, 0F2H, 0F4H, 0F8H  ; 数码管位码
 
 STATE       DB  0
-
+COUNT       DW  0
 
 .CODE
 .STARTUP
@@ -56,10 +56,72 @@ STATE       DB  0
     MOV     AL, AH
     OUT     DX, AL
 
+    CALL    ENTER_STATE_1   ; ENTER STATE 1
+    MOV     DX, I8255ADDC
+    IN      AL, DX
+    TEST    AL, 10H
+    JZ                      ; 1s COUNT NOT COME
+                             ; 1s COUNT COME
+    
+
 ENTER_STATE_1   PROC    NEAR
     MOV     BX, OFFSET STATE
     MOV     [BX], STATE_1
+    ; SET LIGHT
     MOV     AL, STATE_1_LIGHT
     MOV     DX, I8255ADDB
     OUT     DX, AL
-    
+    ; SET COUNTDOWN
+    MOV     BX, OFFSET COUNT
+    MOV     [BX], 30    
+    MOV     BX, OFFSET BUF
+    MOV     [BX], 0
+    MOV     [BX + 1], 0
+    MOV     [BX + 2], 3
+    MOV     [BX + 3], 0
+    CALL    DISPLAY
+
+    RET
+ENTER_STATE_1   ENDP
+
+ENTER_STATE_2   PROC    NEAR
+
+    RET
+ENTER_STATE_2   ENDP
+
+DISPLAY     PROC    NEAR    ; 数码管显示数值，四位数值存放在BUF中
+    MOV     SI, OFFSET BUF
+    MOV     DI, OFFSET BZ
+    MOV     CX, 4
+DISP_L1:
+    MOV     AL, [SI]
+    MOV     BX, OFFSET TAB
+    XLAT
+    MOV     DX, I8255ADDA
+    OUT     DX, AL
+    MOV     AL, [DI]
+    MOV     DX, I8255ADDC
+    OUT     DX, AL
+    CALL    DELAY
+    INC     SI
+    INC     DI
+    MOV     DX, I8255ADDC
+    MOV     AL, 0F0H
+    OUT     DX, AL
+    LOOP    DISP_L1
+    RET
+DISPLAY     ENDP
+
+
+
+DELAY   PROC    NEAR
+    PUSH    CX
+    MOV     CX, 07FFH
+LOP1:
+    LOOP    LOP1
+    POP     CX
+    RET
+DELAY   ENDP
+
+END
+
